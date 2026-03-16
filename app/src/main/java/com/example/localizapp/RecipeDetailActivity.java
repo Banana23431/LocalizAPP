a44487f657ce99f8f5a98656a1f7551d5b99c226
Banana23431 <prikolist098@gmail.com>
2026-03-17 00:04:13 +0500

    Описание изменений: добавлена функциональность таймеров

--- app/src/main/java/com/example/localizapp/RecipeDetailActivity.java
+++ app/src/main/java/com/example/localizapp/RecipeDetailActivity.java
@@ -2,12 +2,15 @@ package com.example.localizapp;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
@@ -19,6 +22,7 @@ import java.io.Reader;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -27,15 +31,34 @@ import com.google.gson.reflect.TypeToken;
 
 public class RecipeDetailActivity extends AppCompatActivity {
 
-    private TextView recipeNameTextView;
-    private TextView recipeIngredientsTextView;
-    private TextView recipeStepsTextView;
+    private TextView recipeNameTextView, recipeIngredientsTextView, recipeStepsTextView, textVideo, textAudio;
     private MediaPlayer mediaPlayer;
-    private Button btnBack, playButton, pauseButton, stopButton,playVideoButton, pauseVideoButton, stopVideoButton;
+    private Button btnBack, playButton, pauseButton, stopButton, playVideoButton, pauseVideoButton, stopVideoButton;
     private VideoView videoView;
+    private TextView timer1TextView, timer2TextView, timer3TextView;
+    private EditText timer2EditText;
+    private Button timer1StartButton, timer1PauseButton, timer1ResetButton,
+            timer2StartButton, timer2PauseButton, timer2ResetButton, timer2SetButton,
+            timer3StartButton, timer3PauseButton, timer3ResetButton;
+
+
+    private CountDownTimer timer1, timer2, timer3;
+
+
+    private long timer1MillisInFuture = 60000;
+    private long timer2MillisInFuture = 60000;
+    private long timer3MillisInFuture = 60000;
+
+    private boolean timer1IsRunning = false;
+    private boolean timer2IsRunning = false;
+    private boolean timer3IsRunning = false;
+
+    private int timer1SoundId = R.raw.timer1_sound;
+    private int timer2SoundId = R.raw.timer2_sound;
+    private int timer3SoundId = R.raw.timer3_sound;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         String currentLanguage = LocaleHelper.getLanguage(this);
         Context context = LocaleHelper.setLocale(this, currentLanguage);
@@ -43,6 +66,8 @@ public class RecipeDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_recipe_detail);
 
         videoView = findViewById(R.id.videoView);
+        textVideo = findViewById(R.id.textVideo);
+        textAudio = findViewById(R.id.textAudio);
         btnBack = findViewById(R.id.btnBack);
         recipeNameTextView = findViewById(R.id.recipeNameTextView);
         recipeIngredientsTextView = findViewById(R.id.recipeIngredientsTextView);
@@ -63,7 +88,101 @@ public class RecipeDetailActivity extends AppCompatActivity {
         playVideoButton.setText(resources.getString(R.string.btn_play_video));
         pauseVideoButton.setText(resources.getString(R.string.btn_pause_video));
         stopVideoButton.setText(resources.getString(R.string.btn_stop_video));
+        textVideo.setText(resources.getString(R.string.textVideo));
+        textAudio.setText(resources.getString(R.string.textAudio));
 
+        timer1TextView = findViewById(R.id.timer1TextView);
+        timer2TextView = findViewById(R.id.timer2TextView);
+        timer3TextView = findViewById(R.id.timer3TextView);
+
+        timer2EditText = findViewById(R.id.timer2EditText);
+
+        timer1StartButton = findViewById(R.id.timer1StartButton);
+        timer1PauseButton = findViewById(R.id.timer1PauseButton);
+        timer1ResetButton = findViewById(R.id.timer1ResetButton);
+
+        timer2StartButton = findViewById(R.id.timer2StartButton);
+        timer2PauseButton = findViewById(R.id.timer2PauseButton);
+        timer2ResetButton = findViewById(R.id.timer2ResetButton);
+        timer2SetButton = findViewById(R.id.timer2SetButton);
+
+        timer3StartButton = findViewById(R.id.timer3StartButton);
+        timer3PauseButton = findViewById(R.id.timer3PauseButton);
+        timer3ResetButton = findViewById(R.id.timer3ResetButton);
+
+        timer1StartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startTimer(1);
+            }
+        });
+
+        timer1PauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pauseTimer(1);
+            }
+        });
+
+        timer1ResetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetTimer(1);
+            }
+        });
+
+        timer2StartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startTimer(2);
+            }
+        });
+
+        timer2PauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pauseTimer(2);
+            }
+        });
+
+        timer2ResetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetTimer(2);
+            }
+        });
+
+        timer2SetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setTimer2();
+            }
+        });
+
+        timer3StartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startTimer(3);
+            }
+        });
+
+        timer3PauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pauseTimer(3);
+            }
+        });
+
+        timer3ResetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetTimer(3);
+            }
+        });
+
+        updateTimerText(1);
+        updateTimerText(2);
+        updateTimerText(3);
 
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -94,6 +213,181 @@ public class RecipeDetailActivity extends AppCompatActivity {
         }
     }
 
+
+    private void startTimer(int timerNumber) {
+        if (timerNumber == 1) {
+            if (timer1IsRunning) {
+                return;
+            }
+            timer1IsRunning = true;
+
+            timer1 = new CountDownTimer(timer1MillisInFuture, 1000) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    timer1MillisInFuture = millisUntilFinished;
+                    updateTimerText(1);
+                    updateTimerColor(1);
+                }
+
+                @Override
+                public void onFinish() {
+                    timer1IsRunning = false;
+                    updateTimerText(1);
+                    timer1TextView.setTextColor(Color.BLACK);
+                    playSound(timer1SoundId);
+                }
+            }.start();
+        } else if (timerNumber == 2) {
+            if (timer2IsRunning) {
+                return;
+            }
+            timer2IsRunning = true;
+
+            timer2 = new CountDownTimer(timer2MillisInFuture, 1000) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    timer2MillisInFuture = millisUntilFinished;
+                    updateTimerText(2);
+                    updateTimerColor(2);
+                }
+
+                @Override
+                public void onFinish() {
+                    timer2IsRunning = false;
+                    updateTimerText(2);
+                    timer2TextView.setTextColor(Color.BLACK);
+                    playSound(timer2SoundId);
+                }
+            }.start();
+        } else if (timerNumber == 3) {
+            if (timer3IsRunning) {
+                return;
+            }
+            timer3IsRunning = true;
+
+            timer3 = new CountDownTimer(timer3MillisInFuture, 1000) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    timer3MillisInFuture = millisUntilFinished;
+                    updateTimerText(3);
+                    updateTimerColor(3);
+                }
+
+                @Override
+                public void onFinish() {
+                    timer3IsRunning = false;
+                    updateTimerText(3);
+                    timer3TextView.setTextColor(Color.BLACK);
+                    playSound(timer3SoundId);
+                }
+            }.start();
+        }
+    }
+
+    private void pauseTimer(int timerNumber) {
+        if (timerNumber == 1) {
+            if (timer1IsRunning) {
+                timer1.cancel();
+                timer1IsRunning = false;
+            }
+        } else if (timerNumber == 2) {
+            if (timer2IsRunning) {
+                timer2.cancel();
+                timer2IsRunning = false;
+            }
+        } else if (timerNumber == 3) {
+            if (timer3IsRunning) {
+                timer3.cancel();
+                timer3IsRunning = false;
+            }
+        }
+    }
+
+    private void resetTimer(int timerNumber) {
+        if (timerNumber == 1) {
+            timer1MillisInFuture = 60000;
+            updateTimerText(1);
+            timer1TextView.setTextColor(Color.BLACK);
+            timer1IsRunning = false;
+            if (timer1 != null) {
+                timer1.cancel();
+            }
+        } else if (timerNumber == 2) {
+            timer2MillisInFuture = 60000;
+            updateTimerText(2);
+            timer2TextView.setTextColor(Color.BLACK);
+            timer2IsRunning = false;
+            if (timer2 != null) {
+                timer2.cancel();
+            }
+        } else if (timerNumber == 3) {
+            timer3MillisInFuture = 60000;
+            updateTimerText(3);
+            timer3TextView.setTextColor(Color.BLACK);
+            timer3IsRunning = false;
+            if (timer3 != null) {
+                timer3.cancel();
+            }
+        }
+    }
+
+
+    private void setTimer2() {
+        try {
+            timer2MillisInFuture = Long.parseLong(timer2EditText.getText().toString()) * 60000;
+            updateTimerText(2);
+        } catch (NumberFormatException e) {
+        }
+    }
+    private void updateTimerText(int timerNumber) {
+        long millisInFuture = 0;
+        TextView timerTextView = null;
+
+        if (timerNumber == 1) {
+            millisInFuture = timer1MillisInFuture;
+            timerTextView = timer1TextView;
+        } else if (timerNumber == 2) {
+            millisInFuture = timer2MillisInFuture;
+            timerTextView = timer2TextView;
+        } else if (timerNumber == 3) {
+            millisInFuture = timer3MillisInFuture;
+            timerTextView = timer3TextView;
+        }
+
+        int minutes = (int) (millisInFuture / 1000) / 60;
+        int seconds = (int) (millisInFuture / 1000) % 60;
+
+        String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+        timerTextView.setText(timeLeftFormatted);
+    }
+
+    private void updateTimerColor(int timerNumber) {
+        TextView timerTextView = null;
+        long millisInFuture = 0;
+
+        if (timerNumber == 1) {
+            millisInFuture = timer1MillisInFuture;
+            timerTextView = timer1TextView;
+        } else if (timerNumber == 2) {
+            millisInFuture = timer2MillisInFuture;
+            timerTextView = timer2TextView;
+        } else if (timerNumber == 3) {
+            millisInFuture = timer3MillisInFuture;
+            timerTextView = timer3TextView;
+        }
+
+        if (millisInFuture < 60000) {
+            timerTextView.setTextColor(Color.RED);
+        } else {
+            timerTextView.setTextColor(Color.BLACK);
+        }
+    }
+
+
+    private void playSound(int soundId) {
+        MediaPlayer mp = MediaPlayer.create(this, soundId);
+        mp.start();
+    }
 
     private Recipe loadRecipeById(int recipeId) {
         List<Recipe> recipeList = loadRecipes();
